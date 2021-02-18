6899c339313e140cac988ee5c4528e5e7deb9624
Raihannoob <you@example.com>
2021-02-18 21:24:20 +0600

    Task2 a

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK_A/task1.php
@@ -0,0 +1,18 @@
+<?php
+
+	
+
+	if(isset($_POST['submit'])){
+
+		$Ename 		= $_POST['myemail'];
+
+		if($Ename == ""){
+			echo "null submission...";
+		}else{
+			echo "Email:".$Ename;
+		}
+
+	}else{
+		echo "invalid request...";
+	}
+?> 
