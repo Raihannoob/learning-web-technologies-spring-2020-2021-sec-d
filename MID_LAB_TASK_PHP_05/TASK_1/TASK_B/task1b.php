147251af86e8029c06f162979b24671ca71a9afd
Raihannoob <you@example.com>
2021-02-18 20:06:20 +0600

    Task_1 Task_B

---
+++ MID_LAB_TASK_PHP_05/TASK_1/TASK_B/task1b.php
@@ -0,0 +1,34 @@
+<?php
+	if(isset($_POST['submit'])){
+
+		$name = $_POST['myname'];
+
+		if($name == ""){
+			echo "null submission...";
+		}else{
+			echo "Name:".$name;
+
+
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Name Input</title>
+</head>
+
+<body>
+    <form method="post" action="#">
+        <fieldset>
+            <legend>Name</legend>
+            <input type="text" name="myname" value=""> <br>
+            <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+
+</body>
+
+</html>
