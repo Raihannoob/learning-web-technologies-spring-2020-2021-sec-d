7c8246000578a75d8f09dd07b923ceb2d9480f02
Raihannoob <you@example.com>
2021-02-25 21:21:55 +0600

    change password

---
+++ LAB_TASk_5.2_SESSION/changePassword.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<table border="1" width="100%" cellspacing="0">
+		<tr>
+			<td align="right">
+				<a href="publichome.php"> <img src="logo.png" align="left"> </a> 
+				<a href="publichome.php"> Home </a> 
+				&nbsp | &nbsp
+				<a href="login.php"> Login </a>
+				&nbsp | &nbsp
+				<a href="regestration.php"> Registration </a>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<br>
+				<form>
+		<table border="0" cellspacing="0" cellpadding="5">
+			<tr>
+				<td colspan="2">
+				<form method="POST" action="" >
+					<fieldset >
+						<legend>CHANGE PASSWORD</legend>
+						Current Password<br 
+						/>
+						<input type="password" /><br 
+						/>
+						New Password<br />
+						<input type="password" /><br
+						 />
+						Retype New Password<br 
+						/>
+						<input type="password"
+						/>								
+						<hr 
+						/>
+						<input type="button" value="Change" 
+						/>     
+						<a href="">Home</a>						
+					</fieldset>
+					</form>
+				</td>
+			</tr>
+		</table>
+	</form>
+			<br>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<center> Copyright &copy 2017 </center>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
